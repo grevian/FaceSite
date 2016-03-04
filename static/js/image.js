c92583f4b7646d0e02fb7fc5169c525b42bbc90b
grevian <grevian+github@gmail.com>
2016-03-04 02:06:28 -0500

    Fix bugs in JS field population

--- static/js/image.js
+++ static/js/image.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     var image_id = window.location.pathname.substr(1).split("/")[1];
-    var preloaded = $("#preloaded-marker").val()
-    if (image_id && preloaded != 0) {
+    var preloaded = $("#preloaded-marker").text()
+    if (image_id && preloaded == "0") {
         pollImage(image_id)
     }
 });
